c98c5a3a6b7aef074ea2e2ecf1848c2f18d41d65
Chanawee Jampeecharoensuk <121846037+Mvptong@users.noreply.github.com>
2023-02-05 00:39:13 +0700

    Update gcs_to_bq.py

--- gcs_to_bq.py
+++ gcs_to_bq.py
@@ -7,30 +7,20 @@ from airflow.utils.dates import days_ago
 import pandas as pd
 import requests
 
-MYSQL_CONNECTION = "mysql_default"   # ชื่อของ connection ใน Airflow ที่เซ็ตเอาไว้
+MYSQL_CONNECTION = "mysql_default"   
 CONVERSION_RATE_URL = "https://r2de2-workshop-vmftiryt6q-ts.a.run.app/usd_thb_conversion_rate"
 
-# path ที่จะใช้
+# path 
 mysql_output_path = "/home/airflow/gcs/data/audible_data_merged.csv"
 conversion_rate_output_path = "/home/airflow/gcs/data/conversion_rate.csv"
 final_output_path = "/home/airflow/gcs/data/output.csv"
 
 
 def get_data_from_mysql(transaction_path):
-    # รับ transaction_path มาจาก task ที่เรียกใช้
-
-    # เรียกใช้ MySqlHook เพื่อต่อไปยัง MySQL จาก connection ที่สร้างไว้ใน Airflow
     mysqlserver = MySqlHook(MYSQL_CONNECTION)
-    
-    # Query จาก database โดยใช้ Hook ที่สร้าง ผลลัพธ์ได้ pandas DataFrame
     audible_data = mysqlserver.get_pandas_df(sql="SELECT * FROM audible_data")
     audible_transaction = mysqlserver.get_pandas_df(sql="SELECT * FROM audible_transaction")
-
-    # Merge data จาก 2 DataFrame เหมือนใน workshop1
     df = audible_transaction.merge(audible_data, how="left", left_on="book_id", right_on="Book_ID")
-
-    # Save ไฟล์ CSV ไปที่ transaction_path ("/home/airflow/gcs/data/audible_data_merged.csv")
-    # จะไปอยู่ที่ GCS โดยอัตโนมัติ
     df.to_csv(transaction_path, index=False)
     print(f"Output to {transaction_path}")
 
@@ -39,15 +29,12 @@ def get_conversion_rate(conversion_rate_path):
     r = requests.get(CONVERSION_RATE_URL)
     result_conversion_rate = r.json()
     df = pd.DataFrame(result_conversion_rate)
-
-    # เปลี่ยนจาก index ที่เป็น date ให้เป็น column ชื่อ date แทน แล้วเซฟไฟล์ CSV
     df = df.reset_index().rename(columns={"index": "date"})
     df.to_csv(conversion_rate_path, index=False)
     print(f"Output to {conversion_rate_path}")
 
 
 def merge_data(transaction_path, conversion_rate_path, output_path):
-    # อ่านจากไฟล์ สังเกตว่าใช้ path จากที่รับ parameter มา
     transaction = pd.read_csv(transaction_path)
     conversion_rate = pd.read_csv(conversion_rate_path)
 
@@ -79,7 +66,7 @@ with DAG(
 ) as dag:
 
     dag.doc_md = """
-    # Workshop5: Load to BigQuery ด้วยคำสั่ง GCSToBigQueryOperator ของ Airflow 
+    # Load to BigQuery ด้วยคำสั่ง GCSToBigQueryOperator ของ Airflow 
     เหมาะสำหรับ worker ที่ไม่มี bq command
     Documentation : https://airflow.apache.org/docs/apache-airflow-providers-google/stable/operators/cloud/gcs.html
     Example: https://airflow.apache.org/docs/apache-airflow-providers-google/stable/_modules/airflow/providers/google/cloud/example_dags/example_gcs_to_bigquery.html
@@ -107,7 +94,7 @@ with DAG(
         },
     )
 
-    # TODO: สร้าง t4 ที่เป็น GCSToBigQueryOperator เพื่อใช้งานกับ BigQuery แบบ Airflow และใส่ dependencies
+   
 
     t4 = GCSToBigQueryOperator(
         task_id='gcs_to_bq',
